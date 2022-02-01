c2ab5ffb0767c8bf53001f28d3c3c65e493db54a
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-02-01 13:24:32 +1100

    Resizing

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-"use strict";
+'use strict';
+
+// DOM Elements
+const hourEl = document.getElementById('hour');
+const minuteEl = document.getElementById('minute');
+
+
+
+
+
+
+
+// Time setup
+// Function to update time and change hour and minute text
+const updateTime = () => {
+    const currentTime = new Date();
+    
+    let currentHour = currentTime.getHours();
+    let currentMinute = currentTime.getMinutes();
+    
+    hourEl.textContent = currentHour.toString();
+    minuteEl.textContent = currentMinute.toString();
+}
+
+//Runs the update time function every second
+setInterval(updateTime, 1000);
+updateTime();
